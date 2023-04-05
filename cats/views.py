ba49ee782d276aa6a1fe59f0de8fd866bceafc82
Alexman2505 <alexman2505@gmail.com>
2023-04-05 14:03:03 +0300

    реализовал работу через вьюсеты. все изменения прослеживаются в urls.py и views.py через комментарии в коде

--- cats/views.py
+++ cats/views.py
@@ -1,22 +1,73 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.response import Response
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# @api_view(['GET', 'POST'])
+# def cat_list(request):
+#     if request.method == 'POST':
+#         serializer = CatSerializer(data=request.data, many=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     cats = Cat.objects.all()
+#     serializer = CatSerializer(cats, many=True)
+#     print(f'1                {cats}')
+#     print(f'2  {serializer}')
+#     print(f'3  {serializer.data}')
+#     return Response(serializer.data)
+
+
+####################### 1 Обновлённый views.py ##################################
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class APICat(APIView):
+#     def get(self, request):
+#         cats = Cat.objects.all()
+#         serializer = CatSerializer(cats, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = CatSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+######################### 2 Обновлённый еще раз views.py ########################
+# from rest_framework import generics
+
+# from .models import Cat
+# from .serializers import CatSerializer
+
+
+# class CatList(generics.ListCreateAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+
+# class CatDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Cat.objects.all()
+#     serializer_class = CatSerializer
+
+################### 3 обновленный в третий раз views.py #########################
+from rest_framework import viewsets
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    print(f'1  {cats}')
-    print(f'2  {serializer}')
-    print(f'3  {serializer.data}')
-    return Response(serializer.data)
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,7 +1,50 @@
-from django.urls import include, path
+# from django.urls import include, path
 
-from cats.views import cat_list
+# from cats.views import cat_list
+
+# urlpatterns = [
+#     path('cats/', cat_list, name='cat_list'),
+# ]
+
+
+################ 1 Обновлённый urls.py #######################################
+# from django.urls import include, path
+
+# from cats.views import APICat
+
+# urlpatterns = [
+#     path('cats/', APICat.as_view()),
+# ]
+
+
+################# 2 Обновлённый еще раз urls.py ############################
+# from django.urls import include, path
+
+# from cats.views import CatDetail, CatList
+
+# urlpatterns = [
+#     path('cats/', CatList.as_view()),
+#     path('cats/<int:pk>/', CatDetail.as_view()),
+# ]
+
+
+################### 3 обновленный в третий раз views.py ###################
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from cats.views import CatViewSet
+
+# Создаётся роутер
+router = SimpleRouter()
+# Вызываем метод .register с нужными параметрами
+router.register('cats', CatViewSet)
+# В роутере можно зарегистрировать любое количество пар "URL, viewset":
+# например
+# router.register('owners', OwnerViewSet)
+# Но нам это пока не нужно
 
 urlpatterns = [
-    path('cats/', cat_list),
+    # Все зарегистрированные в router пути доступны в router.urls
+    # Включим их в головной urls.py
+    path('', include(router.urls)),
 ]
